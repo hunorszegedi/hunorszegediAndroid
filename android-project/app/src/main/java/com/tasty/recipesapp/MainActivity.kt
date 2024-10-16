795ccd5351bee80904c0b4a7af47b4c60d6b0038
hunorszegedi <szegedihunorofficial@gmail.com>
2024-10-16 09:28:21 +0300

    lifecylce methods added to the SplashActivity

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: MainActivity created.")
     }
 
     override fun onStart() {
--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "SplashActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,32 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate: SplashActivity created.")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: SplashActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: SplashActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: SplashActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: SplashActivity stopped.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: SplashActivity destroyed.")
+    }
+
 }
