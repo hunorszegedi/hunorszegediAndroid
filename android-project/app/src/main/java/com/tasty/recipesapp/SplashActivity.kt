b5e13579a3181335a829759f17572e23f4db8126
Hunor <szegedihunorofficial@gmail.com>
2024-11-15 00:30:45 +0200

    lifecycle methods added

--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -4,43 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
-    private val TAG = "MainActivity"
+    private val TAG = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate: MainActivity created.")
+        setContentView(R.layout.activity_splash)
+        Log.d(TAG, "onCreate: SplashActivity created.")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart: MainActivity started.")
+        Log.d(TAG, "onStart: SplashActivity started.")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume: MainActivity resumed.")
+        Log.d(TAG, "onResume: SplashActivity resumed.")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause: MainActivity paused.")
+        Log.d(TAG, "onPause: SplashActivity paused.")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG, "onStop: MainActivity stopped.")
+        Log.d(TAG, "onStop: SplashActivity stopped.")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(TAG, "onRestart: MainActivity restarted.")
+        Log.d(TAG, "onRestart: SplashActivity restarted.")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy: MainActivity destroyed.")
+        Log.d(TAG, "onDestroy: SplashActivity destroyed.")
     }
 }
