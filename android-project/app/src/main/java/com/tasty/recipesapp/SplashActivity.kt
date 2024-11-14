d96a2e6499d0cc489b095900eef49a2c1267f300
Hunor <szegedihunorofficial@gmail.com>
2024-11-15 00:49:54 +0200

    lab1 ujra megcsinalva

--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -1,17 +1,41 @@
 package com.tasty.recipesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
 
     private val TAG = "SplashActivity"
+    private val SPLASH_TIME_OUT: Long = 4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Log.d(TAG, "onCreate: SplashActivity created.")
+
+        val logo = findViewById<ImageView>(R.id.app_logo)
+        val appName = findViewById<TextView>(R.id.app_name)
+        val appDescription = findViewById<TextView>(R.id.app_description)
+
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up)
+
+        logo.startAnimation(fadeInAnimation)
+        appName.startAnimation(slideUpAnimation)
+        appDescription.startAnimation(slideUpAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 
     override fun onStart() {
