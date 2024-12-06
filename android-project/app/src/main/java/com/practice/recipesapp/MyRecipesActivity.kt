ef23a6812d2828de4f9540f4c61db3568c2f371a
Hunor <szegedihunorofficial@gmail.com>
2024-12-06 12:25:48 +0200

    labor 10 api-ig befejezve, profil oldal sajat recept es megjelenites megoldva

--- android-project/app/src/main/java/com/practice/recipesapp/MyRecipesActivity.kt
+++ android-project/app/src/main/java/com/practice/recipesapp/MyRecipesActivity.kt
@@ -3,28 +3,35 @@ package com.practice.recipesapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.practice.recipesapp.databinding.ActivityMyRecipesBinding
 
 class MyRecipesActivity : AppCompatActivity() {
-//    adapterrel hagytuk abba!!
-//    ne felejtsd el video url-t is megjeleniteni mindenhol!
+
     private lateinit var binding: ActivityMyRecipesBinding
-//    private lateinit var myRecipesAdapter: MyRecipesAdapter
     private lateinit var recipeList: ArrayList<Recipe>
+    private lateinit var myRecipesAdapter: SearchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyRecipesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        setupRecyclerView()
+        setupRecyclerView()
     }
 
-//    private fun setupRecyclerView() {
-//        recipeList = ArrayList() // Hozd létre a lista adatokat Room Database segítségével
-//
-//        binding.rvMyRecipes.layoutManager = LinearLayoutManager(this)
-//        myRecipesAdapter = MyRecipesAdapter(recipeList)
-//        binding.rvMyRecipes.adapter = myRecipesAdapter
-//    }
+    private fun setupRecyclerView() {
+        val db = Room.databaseBuilder(this, AppDatabase::class.java, "db_name")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+
+        val dao = db.getDao()
+        val userAddedRecipes = dao.getAll()?.filter { it?.category == "User Added" } ?: emptyList()
+
+        recipeList = ArrayList(userAddedRecipes.filterNotNull())
+        binding.rvMyRecipes.layoutManager = LinearLayoutManager(this)
+        myRecipesAdapter = SearchAdapter(recipeList, this)
+        binding.rvMyRecipes.adapter = myRecipesAdapter
+    }
 }
